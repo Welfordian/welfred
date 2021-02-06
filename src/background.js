6bc61189ed50266590c0a7f02befaceac1d3e25e
Joshua Welford <josh@welford.me>
2021-02-06 11:18:42 -0500

    WIP

--- src/background.js
+++ src/background.js
@@ -145,6 +145,14 @@ app.on('ready', () => {
 
   createWindow();
 
+  globalShortcut.register('Control+Shift+I', () => {
+    // When the user presses Ctrl + Shift + I, this function will get called
+    // You can modify this function to do other things, but if you just want
+    // to disable the shortcut, you can just return false
+    return false;
+  });
+
+
   globalShortcut.register('Option+Space', function () {
     if (! mainWindow) {
       createWindow();
